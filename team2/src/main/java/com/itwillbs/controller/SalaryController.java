b3f1a8f9766fb716feae827d1fdea403524de1cd
goeun <sal9room2@gmail.com>
2025-01-07 10:28:01 +0900

    급여입력 오류 해결중

--- team2/src/main/java/com/itwillbs/controller/SalaryController.java
+++ team2/src/main/java/com/itwillbs/controller/SalaryController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.itwillbs.service.SalaryService;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
 
@@ -34,7 +35,13 @@ public class SalaryController {
 	// 관리자 급여입력 내역 조회
 	// 페이지 첫 로딩 시 입력된 급여 테이블 출력
 	@GetMapping("/salaryInput")
-	public String salaryInput(Map<String, Object> map, Model model) {
+	public String salaryInput(Map<String, Object> map, Model model, HttpSession session) {
+		
+		// 관리자 페이지라 관리자 로그인 필수
+//		if (session.getAttribute("id") == null) {
+//            return "redirect:/login"; 
+//        }
+//		
 		log.info("============= salaryInput =============");
 		
 		String id = "admin";
@@ -58,6 +65,7 @@ public class SalaryController {
 	@PostMapping("/salaryInput")
 	@ResponseBody
 	public List<Map<String, Object>> getSalaryInput(@RequestParam Map<String, Object> map) {
+
 		log.info("============= salaryInput POST =============");
 		
 		map.put("id", "admin");
