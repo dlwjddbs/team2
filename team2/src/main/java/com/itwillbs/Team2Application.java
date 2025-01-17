fe620de6f91834fba6e16dff8a194a983207d6a1
ITWILL <useong3754@gmail.com>
2025-01-17 13:25:34 +0900

    war export 설정

--- team2/src/main/java/com/itwillbs/Team2Application.java
+++ team2/src/main/java/com/itwillbs/Team2Application.java
@@ -2,12 +2,19 @@ package com.itwillbs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Team2Application {
+public class Team2Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Team2Application.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Team2Application.class); 
 	}
 
 }
