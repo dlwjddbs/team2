1fb440d38c96c7745b1d1a108fa518b689fd99f5
bukyong <pbg736837@gmail.com>
2025-02-05 18:24:49 +0900

    설비정보등록 페이지 CRUD

--- team2/src/main/java/com/itwillbs/service/EquipmentService.java
+++ team2/src/main/java/com/itwillbs/service/EquipmentService.java
@@ -40,5 +40,79 @@ public class EquipmentService {
 		
 		return resultMap;
 	}
+	
+	@Transactional(rollbackFor = Exception.class)
+	public Map<String, Object> modifyWorkcenter(Map<String, Object> requestData) {
+		List<Map<String, Object>> updatedRows = (List<Map<String, Object>>)requestData.get("updatedRows");
+		List<Map<String, Object>> createdRows = (List<Map<String, Object>>)requestData.get("createdRows");
+
+		Map<String, Object> resultMap = new HashMap<>();
+		
+		Boolean result = true;
+		String message = "modifyToastTest 성공";
+		
+		try {
+			if (createdRows.size() > 0) {
+				equipmentMapper.insertEquipment(createdRows);
+			}
+			
+			if (updatedRows.size() > 0) {
+				equipmentMapper.updateEquipment(updatedRows);
+			}
+		} catch (Exception e) {
+			result = false;
+			message = "modifyToastTest 실패";
+		}
+		
+		resultMap.put("result", result);
+		resultMap.put("message", message);
+		
+		return resultMap;
+	}
+
+	public Map<String, Object> deleteWorkcenter(List<String> idList) {
+		Map<String, Object> resultMap = new HashMap<>();
+		
+		Boolean result = true;
+		String message = "deleteWorkcenter 성공";
+		
+		try {
+			if (idList.size() > 0) {
+				equipmentMapper.deleteEquipment(idList);
+			}
+		} catch (Exception e) {
+			result = false;
+			message = "deleteWorkcenter 실패";
+		}
+		
+		resultMap.put("result", result);
+		resultMap.put("message", message);
+		
+		return resultMap;
+	}
+
+	public Map<String, Object> checkDuplicateEquipCode(Map<String, Object> map) {
+		log.info("============= checkDuplicateEquipCode(Service) =============");
+		Map<String, Object> message = new HashMap<>();
+		
+		String result = "중복된 코드입니다.";
+		String resultCode = "0";
+		
+		try {
+			int resultCnt = equipmentMapper.checkDuplicateEquipCode(map);
+			if (resultCnt == 0) {
+				result = "사용가능한 코드입니다.";
+				resultCode = "1";
+			}
+		} catch (Exception e) {
+			result = "조회 실패. 재시도 하세요.";
+			resultCode = "0";
+		}
+		
+		message.put("result", result);
+		message.put("resultCode", resultCode);
+		
+		return message;
+	}
 
 }
