f6f370d5d19a154b7c857c95fa11b6545e996a0a
BloomingWisdom <flourish.hye@gmail.com>
2025-02-19 16:13:47 +0900

    입고검수 진행중

--- team2/src/main/java/com/itwillbs/controller/PopController.java
+++ team2/src/main/java/com/itwillbs/controller/PopController.java
@@ -1,10 +1,20 @@
 package com.itwillbs.controller;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.itwillbs.service.InspectionService;
+import com.itwillbs.service.OrderService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
@@ -15,6 +25,9 @@ import lombok.extern.java.Log;
 @Log
 public class PopController {
 	
+	private final InspectionService inspectionService;
+	
+	
 	@GetMapping("/workOrder")
 	public String lots(@AuthenticationPrincipal User user, Model model ) {
 		if(user == null) {
@@ -23,5 +36,20 @@ public class PopController {
 		
 		return "/POP/workOrder";
 	}
+	@GetMapping("/inboundOrder")
+	public String inboundOrder(@AuthenticationPrincipal User user, Model model ) {
+		if(user == null) {
+			return "redirect://login";
+		}
+		return "/POP/inbound/inboundOrder";
+	}
+	
+	@PostMapping("/ajax/saveInboundInspection")
+	public String saveInboundInspection(@RequestBody List<Map<String, Object>> rejectionList) {
+		inspectionService.processInboundInspection(rejectionList);
+	    return "redirect:/inboundOrder";
+	}
+
+	
 	
 }
--- team2/src/main/java/com/itwillbs/service/InspectionService.java
+++ team2/src/main/java/com/itwillbs/service/InspectionService.java
@@ -3,6 +3,7 @@ package com.itwillbs.service;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
@@ -15,6 +16,51 @@ import lombok.RequiredArgsConstructor;
 public class InspectionService {
 	private final InspectionMapper inspectionMapper;
 	
+
+	public Map<String, Object> selectInboundList() {
+		Map<String, List<Map<String, Object>>> content = new HashMap<>();
+		Map<String, Object> resultMap = new HashMap<>();
+		
+		Boolean result = true;
+		String message = "selectEquipment 성공";
+		
+		try {
+			List<Map<String, Object>> rejectionList = inspectionMapper.selectInboundList();
+			content.put("contents", rejectionList);
+			resultMap.put("data", content);
+		} catch (Exception e) {
+			result = false;
+			message = "selectEquipment 실패";
+		}
+		
+		resultMap.put("result", result);
+		resultMap.put("message", message);
+		
+		return resultMap;
+	}
+
+	public Map<String, Object> selectInboundDetail(Map<String, Object> map) {
+		Map<String, List<Map<String, Object>>> content = new HashMap<>();
+		Map<String, Object> resultMap = new HashMap<>();
+		
+		Boolean result = true;
+		String message = "selectEquipment 성공";
+		
+		try {
+			List<Map<String, Object>> rejectionList = inspectionMapper.selectInboundDetail(map);
+			content.put("contents", rejectionList);
+			resultMap.put("data", content);
+		} catch (Exception e) {
+			result = false;
+			message = "selectEquipment 실패";
+		}
+		
+		resultMap.put("result", result);
+		resultMap.put("message", message);
+		
+		return resultMap;
+	}
+
 	public Map<String, Object> selectRejectionCode() {
 		Map<String, List<Map<String, Object>>> content = new HashMap<>();
 		Map<String, Object> resultMap = new HashMap<>();
@@ -24,6 +70,114 @@ public class InspectionService {
 		
 		try {
 			List<Map<String, Object>> rejectionList = inspectionMapper.selectRejectionCode();
+			content.put("contents", rejectionList);
+			resultMap.put("data", content);
+		} catch (Exception e) {
+			result = false;
+			message = "selectEquipment 실패";
+		}
+		
+		System.out.println(resultMap);
+		
+		resultMap.put("result", result);
+		resultMap.put("message", message);
+		
+		return resultMap;
+	}
+	
+	public Map<String, Object> insertRejectionCode(List<Map<String, Object>> createdRows) {
+		Map<String, Object> resultMap = new HashMap<>();
+		
+		Boolean result = true;
+		String message = "insertToastTest 성공";
+		
+		try {
+			int cnt = inspectionMapper.countExistingIds(createdRows);
+			if (cnt > 0) {
+				result = false;
+				message = "중복된 ToastTest값";
+			} else {
+				inspectionMapper.insertRejectionCode(createdRows);
+			}
+		} catch (Exception e) {
+			result = false;
+			message = "insertToastTest 실패";
+		}
+		
+		resultMap.put("result", result);
+		resultMap.put("message", message);
+		
+		return resultMap;
+	}
+
+	public Map<String, Object> deleteRejectionCode(List<String> idList) {
+		Map<String, Object> resultMap = new HashMap<>();
+		
+		Boolean result = true;
+		String message = "deleteToastTest 성공";
+		
+		try {
+			inspectionMapper.deleteRejectionCode(idList);
+		} catch (Exception e) {
+			result = false;
+			message = "deleteToastTest 실패";
+		}
+		
+		resultMap.put("result", result);
+		resultMap.put("message", message);
+		
+		return resultMap;
+	}
+
+	public Map<String, Object> updateRejectionCode(List<Map<String, Object>> updatedRows) {
+		Map<String, Object> resultMap = new HashMap<>();
+		
+		Boolean result = true;
+		String message = "updateToastTest 성공";
+		
+		try {
+			inspectionMapper.updateRejectionCode(updatedRows);
+		} catch (Exception e) {
+			result = false;
+			message = "updateToastTest 실패";
+		}
+		
+		resultMap.put("result", result);
+		resultMap.put("message", message);
+		
+		return resultMap;
+	}
+
+	public Map<String, Object> processInboundInspection(List<Map<String, Object>> rejectionList) {
+		Map<String, Object> resultMap = new HashMap<>();
+		
+		Boolean result = true;
+		String message = "processInboundInspection() 성공";
+		
+		try {
+            String poDetailId = rejectionList.get(0).get("PODETAIL_ID").toString();
+            inspectionMapper.mergeInboundInspection(rejectionList);
+            inspectionMapper.deletemissingInboundInspection(poDetailId, rejectionList);
+        } catch (Exception e) {
+            result = false;
+            message = "processInboundInspection() 실패";
+        }
+
+        resultMap.put("result", result);
+        resultMap.put("message", message);
+        
+		return resultMap;
+	}
+
+	public Map<String, Object> selectInboundInspectionList(Map<String, Object> map) {
+		Map<String, List<Map<String, Object>>> content = new HashMap<>();
+		Map<String, Object> resultMap = new HashMap<>();
+		
+		Boolean result = true;
+		String message = "selectEquipment 성공";
+		
+		try {
+			List<Map<String, Object>> rejectionList = inspectionMapper.selectInboundInspectionList(map);
 			content.put("contents", rejectionList);
 			resultMap.put("data", content);
 		} catch (Exception e) {
