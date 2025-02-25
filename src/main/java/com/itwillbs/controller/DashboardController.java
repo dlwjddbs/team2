080744536970fed94d6cb9bd70fe001067b94ade
bukyong <70992081+bukyong@users.noreply.github.com>
2025-02-26 00:31:44 +0900

    대시보드 일별/주별/월별 수정중 (aws)

--- src/main/java/com/itwillbs/controller/DashboardController.java
+++ src/main/java/com/itwillbs/controller/DashboardController.java
@@ -76,30 +76,10 @@ public class DashboardController {
 	}
 	
 	// 막대기 차트(발주(입고))
-	@PostMapping("/dashboard/getPoStackedChart")
-	@ResponseBody
-	public List<Map<String, Object>> getPoStackedChart(@RequestParam Map<String, Object> map) {
-		List<Map<String, Object>> poHistoryStackedBar = dashboardService.getPoHistoryStackedBarChart(map);
-		
-		return poHistoryStackedBar;
-	}
 	
 	// 거래처별 발주량 및 불량률 조회 (grid)
-//	@PostMapping("/dashboard/getClientPo")
-//	@ResponseBody
-//	public List<Map<String, Object>> getClientPo(@RequestParam Map<String, Object> map) {
-//		List<Map<String, Object>> clientPo = dashboardService.getClientPo(map);
-//		
-//		return clientPo;
-//	}
 	
 	// 거래처별 불량률 차트
-	@PostMapping("/dashboard/getClientInbound")
-	@ResponseBody
-	public List<Map<String, Object>> getClientInboundChart(@RequestParam Map<String, Object> map) {
-		List<Map<String, Object>> clientInbound = dashboardService.getClientInboundChart(map);
-		
-		return clientInbound;
-	}
+
 	
 }
